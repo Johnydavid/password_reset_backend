e6680a2f95a62d40b2cb1e0ab9b4824e9e74b1f2
Johnydavid <dropin2johny@gmail.com>
2023-05-27 19:24:19 +0530

    c

--- models/user.js
+++ models/user.js
@@ -9,22 +9,23 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
 });
 
-// userSchema.methods.generateAuthToken = function(){
-//     const token = jwt.sign({_id:this._id}, process.env.JWTPRIVATEKEY, {expiresIn:"7d"});
-//     return token;
-// }
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id}, process.env.JWTPRIVATEKEY, {expiresIn:"7d"});
+    return token;
+}
 
 const User = mongoose.model("user", userSchema);
 
-const validate = (user)=>{
+const validate = (data)=>{
     const schema = Joi.object({
         name: Joi.string().required(),
-        email: Joi.string().email.required(),
-        password: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: passwordComplexity().required()
     });
-    return schema.validate(user);
+    return schema.validate(data);
 }
 
 module.exports = {User, validate};
--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -1,1 +1,106 @@
-const {user} = require("../models/user");
+const {User} = require("../models/user");
+const Token = require("../models/token");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
+const Joi = require("joi");s
+const express = require("express");
+const router = express.Router();
+const passwordComplexity = require("joi-password-complexity");
+const bcrypt = require("bcrypt");
+
+//Send Password Links
+
+router.post("/", async (req, res)=>{
+    try{
+        const emailSchema = Joi.object({email: Joi.string().email().required().label("Email")});
+        const {error} = emailSchema.validate(req.body);
+        if(error)
+        return res.send(400).send(error.details[0].message)
+
+        const user = await User.findOne({email: req.body.email});
+        if(!user)
+        return res.send(400).send("user does not exist");
+
+        let token = await Token.findOne({userId: user._id});
+
+        if(!token){
+            token = await new Token({
+                userId : user._id,
+                token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+        }
+
+        const url = `${process.env.BASE_URL}/password-reset/${user_id}/${token.token}/`
+        await sendEmail(user.email, "Password reset", url);
+
+       res.status(200).send("password reset link sent to your email");
+      
+
+    } catch(error){
+        res.status(500).send("Internal server Error");
+        console.log(error);
+    }
+});
+
+
+// Verify password reset link
+
+router.get("/:id/:token", async (req, res)=>{
+    try{
+        const user = await User.findOne({_id:req.params.id});
+        if(!user)
+        return
+        res.status(400).send({message: "Invalid Link"});
+
+        const token = await Token.findOne({
+            userId:user._id,
+            token: req.params.token,
+        })
+
+        if(!token)
+        return res.status(400).send({message: "Invalid Link"})
+
+        res.status(200).send("Valid Link")
+
+
+    } catch(error){
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+
+// set New Password
+
+router.post("/:id/:token", async(req, res)=>{
+    try{
+        const passwordSchema = Joi.object({
+            password:passwordComplexity().required().label("Password"),
+        });
+        const{error} = passwordSchema.validate(req.body);
+        if(error)
+        return res.status(400).send({message: error.details[0].message});
+
+        const user  = await User.findOne({_id: req.params.id});
+        if(!user) return res.status(400).send({message : "Invalid Link"});
+
+        const token = await Token.findOne({
+            userId : user._id,
+            token : req.params.token,
+        })
+        if(!token) return res.send(400).send({message: "Invalid Link"});
+        if (!user.verified) user.verified = true;
+
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        user.password = hashPassword;
+        await user.save();
+        await token.remove();
+
+    }catch(error){
+        res.status(500).send({message: "Internal Server Error"});
+    }
+})
+
+module.exports = router;
+
